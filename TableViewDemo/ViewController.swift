c11e6ed55eb473ac45c06d3d4254ce271ab7e822
Yang Lu <leon@Yangs-MacBook-Pro.local>
2018-02-11 07:48:09 -0800

    demo example: Adding a Table View and Data Source
    Module 4.3

--- TableViewDemo/ViewController.swift
+++ TableViewDemo/ViewController.swift
@@ -8,7 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel!.text = "This is row \(indexPath.row)"
+        return cell
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +29,5 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
